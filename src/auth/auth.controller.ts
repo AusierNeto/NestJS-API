c941b1e6ec859a73e0940868947f5fc1916fe82e
Ausier Abrunhosa Furtado de Mendonca Neto <ausier.neto@eldorado.org.br>
2026-01-23 18:10:05 -0300

    WIP: Add auth controller with login endpoint

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/login')
+  postLogin(@Body() user: LoginDto) {
+    return this.authService.login(user);
+  }
+
+}
