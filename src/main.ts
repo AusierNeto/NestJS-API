ee5b9f9744494c5e79db21dda6781a5088fb4e59
AusierNeto <ausierneto@gmail.com>
2026-02-18 22:07:27 -0400

    WIP: Add validator on main file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const config = new DocumentBuilder()
@@ -10,6 +11,15 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // whitelist: true, // Remove propriedades que não estão no DTO
+      // forbidNonWhitelisted: true, // Lança erro se enviarem campos extras
+      transform: true, // Converte tipos automaticamente (ex: string id para number)
+    }),
+  );
+
   const swaggerDocument = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('swagger', app, swaggerDocument);
